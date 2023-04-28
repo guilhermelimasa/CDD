7b2c38e6e9317d2ee05534695c74555d4140bf72
Guilherme_Lima <99773466+guilhermelimasa@users.noreply.github.com>
2023-04-27 23:26:09 -0300

    Add files via upload

---
+++ CDD revisão 001.py
@@ -0,0 +1,4 @@
+x = int(input("digite um número: "))
+y = int(input("digite outro número: "))
+media = (x+y)/2
+print(media)
---
+++ CDD revisão 002.py
@@ -0,0 +1,8 @@
+numero = float(input('digite um número aqui: '))
+if numero != 0:
+    if numero > 0:
+        print('número positivo')
+    else:
+        print('númuero negativo')
+else:
+    print('digite um número diferente de zero')
