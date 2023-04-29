fabd0cb9b7ac4a0894b3629a88604c0acb28f85e
Guilherme_Lima <99773466+guilhermelimasa@users.noreply.github.com>
2023-04-29 12:40:49 -0300

    Add files via upload

---
+++ CDD revisão 006.py
@@ -0,0 +1,5 @@
+l = float(input("Quantos litros tem sua caixa d'água?"))
+balde = 0
+while balde == l:
+    balde = float(input("despeje um balde d'água com quantos litros ? "))
+print("caixa d'água cheia!!")
