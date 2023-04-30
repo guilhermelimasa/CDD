f9c0a88d7d97ac70ab94c56d647bd657ff680fe6
Guilherme_Lima <99773466+guilhermelimasa@users.noreply.github.com>
2023-04-30 15:49:35 -0300

    Add files via upload

---
+++ CDD revisão 013.py
@@ -0,0 +1,7 @@
+x = int(input("digite um número: "))
+y = int(input("digite outro número: "))
+soma = x + y
+sub = x - y
+mult = x * y
+exp = x ** y
+print(f"{x} mais {y} é igual a {soma}, a subtração dos mesmos é {sub}, o produto deles é {mult}, e {x} elevado a {y}, é{exp}")
