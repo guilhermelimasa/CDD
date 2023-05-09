993947bcb75d530f9abba86d443106f89be8533f
Guilherme_Lima <99773466+guilhermelimasa@users.noreply.github.com>
2023-05-09 17:25:42 -0300

    Add files via upload

--- ex010.py
+++ ex010.py
@@ -1,34 +1,4 @@
-c = 1
-escolha = 0
-while escolha != 6:
-    c = c +1
-    n1 = float(input("digite o primeiro número: "))
-    n2 = float(input("digite o segundo número: "))
-    while True:
-        print(""""ESCOLHA UMA OPÇÃO:
-        (1)SOMA
-        (2)SUBTRAÇÃO
-        (3)MULTIPLICAÇÃO
-        (4)DIVISÃO
-        (5)ESCOLHER NOVOS NÚMEROS
-        (6)ENCERRAR PROGRAMA""")
-
-        escolha = float(input("digite um número: "))
-
-        if escolha == 1:
-            soma = n1 + n2
-            print(f"a soma entre {n1} e {n2} é {soma}")
-        elif escolha == 2:
-            sub = n1 - n2
-            print(f"a divisão entre {n1} e {n2} é {div}")
-        elif escolha == 3:
-            mult = n1 * n2
-            print(f"a multiplicação entre {n1} e {n2} é {mult}")
-        elif escolha == 4:
-            div = n1 / n2
-            print(f"a divisão entre {n1} e {n2} é {div}")
-        elif escolha == 5:
-            break
-        elif escolha == 6:
-            print("programa encerrado!!!")
-            break
+def estoque(nome,quantidade,valor):
+    return quantidade * valor
+total = estoque("fubá",30,3.5)
+print(total)
