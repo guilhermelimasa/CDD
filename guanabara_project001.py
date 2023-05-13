cfd9b607826c187a44db9a3f1e7d1735110ebf68
Guilherme_Lima <99773466+guilhermelimasa@users.noreply.github.com>
2023-05-13 15:45:05 -0300

    Delete guanabara_project001.py

--- guanabara_project001.py
+++
@@ -1,7 +0,0 @@
-arrai = 'o rato roeu a roupa do rei de roma'
-while True:
-    letra = input("digite uma letra: ")
-    print(f"a letra {letra} aparece {arrai.count(letra)} veze(s)")
-    pergunta = input("vc deseja ver uma nova letra?")
-    if pergunta != 'sim':
-        break
