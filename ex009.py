0601e312f80b1cfead1342211c30f6bf205112d6
Guilherme_Lima <99773466+guilhermelimasa@users.noreply.github.com>
2023-05-16 21:26:44 -0300

    Add files via upload

--- ex009.py
+++ ex009.py
@@ -1,8 +1,29 @@
-texto = 'O rato roeu a roupa do rei de roma'
-def L (text):
-    cont = 0
-    for c in texto:
-        if c in "aeiouAEIOU":
-            cont += 1
-    print(f"o número de vogais é {cont}")
-L(texto)
+#if (n / 1) % 2 == 0:
+#     if (n/2) % 2 == 0:
+      #else:
+      #print(f"{n} é um número primo")
+def kdena(numero):
+    while True:
+        n = int(input("digite um número: "))
+        if n % 2 == 0:
+            if n == 2:
+                print("este número é primo")
+            else:
+                print(f"este número n é primo.")
+        elif n % 3 == 0:
+            if n == 3:
+                print("este número é primo.")
+            else:
+                print(f"este número n é primo.")
+        elif n % 5 == 0:
+            if n == 5:
+                print("este número é primo.")
+            else:
+                print(f"este número n é primo.")
+        else:
+            print("este número é primo.")
+        pergunta = input("vc deseja ver outro número? ")
+        if pergunta != 'sim':
+            break
+
+kdena(1)
