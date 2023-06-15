faf992e8fe0d99a68bb39185a394e294da201a31
Guilherme_Lima <99773466+guilhermelimasa@users.noreply.github.com>
2023-06-15 16:55:04 -0300

    Add files via upload

--- ex002.py
+++ ex002.py
@@ -7,11 +7,24 @@ banco = mysql.connector.connect(
     database='ESCOLATURMAC'
 )
 print(banco)
+
 meucursor = banco.cursor()
-pesquisa = 'select * from funcionarios;'
-meucursor.execute(pesquisa)
-resultado = meucursor.fetchall()
-for x in resultado:
-    print(x)
+#pesquisa = 'select * from funcionarios;'
+#meucursor.execute(pesquisa)
+#resultado = meucursor.fetchall()
+#for x in resultado:
+#    print(x)
+
+
+nome1 = "Alberto lima"
+telefone1 = "81951535984"
+turma1 = "C"
+media1 = 8.95
+sql = "insert into alunos (NOME, TELEFONE, TURMA, MEDIA) values (%s,%s,%s,%s)"
+data = (nome1, telefone1, turma1, media1)
+meucursor.execute(sql, data)
+banco.commit()
+userid = meucursor.lastrowid
+print(userid)
 meucursor.close()
 banco.close()
